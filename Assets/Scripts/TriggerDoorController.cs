677c6fa2290cdcdd28a5127d95d5d3dc39896e14
Liam Megahy <liam.megahy@gmail.com>
2022-01-29 11:28:11 +0000

    Door Work in Progress

---
+++ Assets/Scripts/TriggerDoorController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDoorController : MonoBehaviour
+{
+    [SerializeField] private Animator myDoor = null;
+    [SerializeField] private bool openTrigger = false;
+    [SerializeField] private bool closeTrigger = false;
+
+    void Update(){
+    	if (Input.GetKey("f")){
+    		myDoor.Play("DoorOpen", 0, 0.0f);
+    	}
+    }
+}
+
+//     private void onTriggerEnter(Collider other){
+//     	if(other.CompareTag("Player")){
+//     		if(openTrigger){
+//     			myDoor.Play("DoorOpen", 0, 0.0f);
+//     		}
+//     		else if (closeTrigger){
+//     			myDoor.Play("DoorClose", 0, 0.0f);
+//     		}
+//     	}
+//     }
+// }
