bf06824cc7dcc4524d15734179221bb45d368dc0
Liam Megahy <liam.megahy@gmail.com>
2022-01-29 13:51:57 +0000

    More Work

--- Assets/Scripts/MoveScenes2D.cs
+++ Assets/Scripts/MoveScenes2D.cs
@@ -6,10 +6,18 @@ using UnityEngine.SceneManagement;
 public class MoveScenes2D : MonoBehaviour
 {
 	[SerializeField] private string newLevel;
+	private bool player1Ready = false;
+	private bool player2Ready = false;
 
 	void OnTriggerEnter2D(Collider2D other){
 		if(other.CompareTag("Player")){
-			SceneManager.LoadScene(newLevel);
+			player1Ready = true;
 		}
+		else if(other.CompareTag("Player2")){
+			player2Ready = true;
+		}
+		if(player1Ready == true && player2Ready == true){
+		SceneManager.LoadScene(newLevel);
+	}
 	}
 }
