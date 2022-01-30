03329addf52b8b773b22289a70cb0a598801831f
chockslam <64290122+chockslam@users.noreply.github.com>
2022-01-30 13:01:00 +0000

    buttons work but no feedback

--- Assets/Scripts/DoorSystem/LvLpuzzleLogic.cs
+++ Assets/Scripts/DoorSystem/LvLpuzzleLogic.cs
@@ -7,11 +7,29 @@ public class LvLpuzzleLogic : MonoBehaviour
     public GameObject Trigger1;
     public GameObject Trigger2;
 
+    private ButtonTrigger thisScript;
     private ButtonTrigger script1;
     private ButtonTrigger script2;
 
+    private Renderer rend; 
 
+    private void Awake() {
+        rend = GetComponent<Renderer>();
+        thisScript = GetComponent<ButtonTrigger>();
+        script1 = Trigger1.GetComponent<ButtonTrigger>();
+        script2 = Trigger2.GetComponent<ButtonTrigger>();
+    }
     private void Update() {
+
+        if(thisScript.activated){
+            if(script1.activated){
+                script1.activated = false;
+            }
+            if(script2.activated){
+                script2.activated = false;
+            }
+
+        }
         
     }
 }
