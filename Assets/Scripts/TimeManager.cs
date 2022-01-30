d5535a03e399f727081070d2e539cdd41eaffbb3
Liam Megahy <liam.megahy@gmail.com>
2022-01-30 12:14:00 +0000

    Working on timer

---
+++ Assets/Scripts/TimeManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class TimeManager : MonoBehaviour
+{
+	public float startingTime;
+	private Text theText;
+    void Start()
+    {
+        theText = GetComponent<Text>();
+    }
+    void Update()
+    {
+    	startingTime -= Time.deltaTime;
+        theText.text = "" + startingTime;
+    }
+}
